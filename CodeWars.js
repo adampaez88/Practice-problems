9b3d36dc8bb23a705375a69a53b64f4bb74ae92c
github username <github email address>
2020-03-09 23:35:56 -0600

    new code war problem

--- CodeWars.js
+++ CodeWars.js
@@ -74,3 +74,23 @@ function isValidWalk(walk) {
     
     return walk.length === 10 && north === south && east === west;
   }
+
+
+
+
+
+
+
+// Welcome. In this kata, you are asked to square every digit of a number.
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+// Note: The function accepts an integer and returns an integer
+function squareDigits(num){
+    //may the code be with you
+    let newNumber = num.toString().split('')
+    newNumber.forEach(function(element, index, array){
+      array[index] = element * element
+    })
+    return Number(newNumber.join(''))
+  }
+
+  
