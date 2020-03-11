9aeb9e3966b0afea93312b39a9c38ca4c717a175
github username <github email address>
2020-03-11 14:38:01 -0600

    added new question

--- CodeWars.js
+++ CodeWars.js
@@ -137,3 +137,13 @@ function squareDigits(num){
 // alphabetPosition("The sunset sets at twelve o' clock.")
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
 
+function alphabetPosition(text) {
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+      let code = text.toUpperCase().charCodeAt(i)
+      if (code > 64 && code < 91) result += (code - 64) + " ";
+    }
+  
+    return result.slice(0, result.length - 1);
+  }
+  console.log(alphabetPosition("The sunset sets at twelve o' clock."));
