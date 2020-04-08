a924281c25df51ab57df86a354aaef633d524618
github username <github email address>
2020-04-08 00:44:48 -0600

    added JS and Ruby qs

--- questions.rb
+++ questions.rb
@@ -79,3 +79,58 @@ zip(first, second)
 def zip first, second
     first.zip(second).flatten()
 end
+
+
+
+
+
+# This is a beginner friendly kata especially for UFC/MMA fans.
+
+# It's a fight between the two legends: Conor McGregor vs George Saint Pierre in Madison Square Garden. 
+# Only one fighter will remain standing, and after the fight in an interview with Joe Rogan the 
+# winner will make his legendary statement. It's your job to return the right statement 
+# depending on the winner!
+
+# If the winner is George Saint Pierre he will obviously say:
+
+# "I am not impressed by your performance."
+# If the winner is Conor McGregor he will most undoubtedly say:
+
+# "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+# Good Luck!
+
+
+def quote(fighter)
+    fighter.downcase == "george saint pierre" ? "I am not impressed by your performance." 
+    : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+end
+
+
+
+def quote(fighter)
+    {
+      'george saint pierre' => 'I am not impressed by your performance.',
+      'conor mcgregor'      => "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+    }[fighter.downcase]
+  end
+
+
+  def quote(fighter)
+    fighter.casecmp("george saint pierre") == 0 ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  end
+
+
+
+  def quote(fighter)
+    winner = fighter.downcase
+    if winner == 'george saint pierre'
+      return "I am not impressed by your performance."
+    else
+      return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+    end
+  end
+
+
+
+
+  
